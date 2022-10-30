3ec83d6f99c6c1f246f55454b1af0002dec4b5b7
Brennan Coslett <brennan@coslett.org>
2022-10-30 16:10:17 -0500

    Way smaller and faster data storage.
    * switch to msgspec instead of builtin json
    * store values in msgpack bytearrays instead of json files
          msgspec msgpack arrays are >5x faster than json
    * added comments in database.py
    * added example for adding a value to the database
    
    This stores the values in a bytearray that gets written directly into a
    file and can be read out by the database. It requires a `Struct` definition
    for what is in each of the objects in the array is so that it can be
    decoded.

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,14 +1,19 @@
 from database import Database
 
 def main(db):
-    pass
+    rel = {"tacos": 33, "networking":55}
+    crawled = db.build_crawled_url("testurl", rel)
+    db.add(crawled)
+    print(db)
 
 def cleanup(db):
     db.cleanup()
 
 if __name__ == "__main__":
     # Actual script execution goes in here
-    db = Database()
+    # Database uses input_file for output file when output_file doesn't
+    # exist yet.
+    db = Database(input_file="test_database.msgpack")
     try:
         main(db)
     finally:
--- crawler/database.py
+++ crawler/database.py
@@ -1,41 +1,172 @@
 #! /usr/bin/env python3
-import json
+
+# msgspec dependencies for storing the data.
+from msgspec import Struct
+from msgspec.msgpack import Decoder, Encoder
+from typing import List
+# Dependencies for reading the byte information from
+# the save file.
 from pathlib import Path
+# Handling timestamp information for data in Database.
+from datetime import datetime, timezone
 
+############### MsgPack Structures for saving the data ###############
+class CrawledURL(Struct, array_like=True):
+    """A msgpack struct describing the output of a crawled URL"""
+    url: str
+    timestamp: datetime
+    relevance: dict
+
+############### Database object ###############
 class Database():
+    """
+    Object which handles keeping the state of all the threads for WebCrawling.
+    It also handles reading to and from the storage file.
+    """
 
-    def __init__(self, storage_file="database.json"):
-        self.file = storage_file
-        self.data = self.read_to_dict(self.file)
-    
+    def __str__(self):
+        """
+        Provides a better way to represent the object
+        when printed out using print().
+        """
+        to_print = "Database"
+        for crawled_url in self.data:
+            to_print += f"\n  url: {crawled_url.url}\n    timestamp: {crawled_url.timestamp}\n    relevance: {crawled_url.relevance}"
+        to_print += "\n"
+        return to_print
+
+    def __init__(self, input_file="database.msgpack", output_file=None):
+        """
+        Initializes the object, with values for encoding/decoding the msgpack
+        arrays as well as handling input/output file names.
+        """
+        self.input_file = input_file
+        if output_file is not None:
+            self.output_file = output_file
+        else:
+            self.output_file = self.input_file
+        self.encoder = Encoder()
+        self.decoder = Decoder(List[CrawledURL])
+        self.data = self.decode_file(self.input_file)
+        self.read_only = False # Flag to ignore writing to a file, stops overwriting
+
     def cleanup(self):
+        """
+        Provides a function for all tasks that need to be done to
+        cleanly save all data in progress and updates the file on disk.
+        """
         self.write_to_disk()
-    
-    def read_to_dict(self, file):
+
+    def decode_file(self, file):
+        """
+        Attempts to decode the Database.decoder object.
+        It marks the database as read_only when it cannot be correctly
+        parsed otherwise the self.write_to_disk will overwrite the existing database
+        with an empty array.
+        """
+        array = []
         if Path(file).exists():
             try:
-                dict = json.loads(file)
+                data = Path(file).read_bytes()
+                array = self.decoder.decode(data)
             except:
-                dict = {}
-        return dict
+                pass
+            if array == []:
+                print("Decode failed, leaving original database")
+                self.read_only = True
 
-    def convert_to_json(self):
-        json_lines = json.dumps(self.data, indent=4, sort_keys=True)
-        return json_lines
+        else:
+            print(f"File with name {file} not found.")
+        return array
+
+    def msgspec_current_time(self):
+        """
+        Provides a datetime.datetime object to be used by the
+        database to determine when the values in the database are stale.
+        This special version of datetime.now provides the tz.info value
+        that msgspec requires to convert it to data that can be serialized.
+        """
+        return datetime.now(timezone.utc)
+
+    def build_crawled_url(self, url: str, relevance: dict):
+        """
+        To correctly serialize the data, we need timestamps with tzinfo embedded.
+        This function takes in the url and the relevance dictionary and returns a
+        CrawledURL object with a valid timestamp
+        """
+        return CrawledURL(url=url, timestamp=self.msgspec_current_time(), relevance=relevance)
 
     def write_to_disk(self):
-        output = self.convert_to_json()
-        # self.convert_to_json always outputs a single string
-        with open(self.file, "w+") as file:
-            file.write(output)
+        """
+        Output the encoded data to a file on disk.
+        """
+        if not(self.read_only):
+            output = self.encoder.encode(self.data)
+            # self.encode always outputs a single string
+            with open(self.output_file, "wb+") as file:
+                file.write(output)
 
-    def add(self, keyword, value):
-        keyword = str(keyword)
-        if keyword not in self.data.keys():
-            self.data[keyword] = []
-        self.data[keyword].append(value)
-    
-    def remove(self, keyword, value):
-        keyword = str(keyword)
-        if keyword in self.data.keys():
-            self.data[keyword].pop(value)
+    def add(self, object: CrawledURL, force_add = False):
+        """
+        Adds a URL to the list if it isn't already in there.
+        It shouldn't already be there as that means we crawled
+        the same page twice, unless we loaded an existing database
+        from a file. This can be overwritten with the `force_add` flag.
+        """
+        if not(self.read_only):
+            in_database = self.url_in_database(object.url)
+            if in_database == -1 or force_add:
+                if force_add:
+                    print(f"Forcing update of {object.url} in database.")
+                    if in_database != -1:
+                        self.data.pop(in_database)
+                self.data.append(object)
+            else:
+                print(f"URL {object.url} is already in database. Did we crawl this twice?")
+
+    def url_in_database(self, url: str):
+        """
+        Returns index of a CrawledURL if it already exists in the database
+        else -1.
+        """
+        for crawled in self.data:
+            if crawled.url == url:
+                return self.data.index(crawled)
+        return -1
+
+    def data_stale(self, url: str):
+        """
+        Check if the timestamp on the given url is greater than 5 days
+        """
+        for crawled_url in self.data:
+            if crawled_url.url == url:
+                timediff = datetime.now() - crawled_url.timestamp
+                days_old = timediff.days()
+                if days_old > 5:
+                    return True
+                else:
+                    break
+        return False
+
+    def remove(self, object):
+        """
+        Accepts either a string url or a CrawledURL object,
+        finds it in the database if it exists, removes it from the array,
+        and returns it.
+        """
+        idx = None
+        url_of_interest = None
+        if type(object) is CrawledURL:
+            idx = self.data.index(object)
+            url_of_interest = object.url
+        else:
+            url_of_interest = object
+            # Remove it with a string url value instead of the whole object
+            for crawled_url in self.data:
+                if crawled_url.url == object:
+                    idx = self.data.index()
+        if idx is None:
+            print(f"URL {url_of_interest} not in database.")
+            return None
+        else:
+            return self.data.pop(idx)
