8c9375cf67ddb9f82e7d0e6bab98f3c777adc1b2
Kaylee Swenson <kheides@gmail.com>
2022-12-07 10:03:23 -0600

    add init files

---
+++ gui/__main__.py
@@ -0,0 +1,34 @@
+import PySimpleGUI as sg
+import sys
+sys.path.append('..')
+from crawler.database import urls_with_keyword
+
+sg.theme('BluePurple')
+   
+data = url_with_keyword()
+
+layout = [[sg.Text('Search Keyword or Phrase:'),
+           sg.Text(size=(15,1), key='-OUTPUT-')],
+          [sg.Input(key='-IN-')],
+          [sg.Button('Search'), sg.Button('Exit')],
+          [sg.Table(data, justification='left', key='-TABLE-')]]
+  
+window = sg.Window('Introduction', layout)
+
+keyword = sg.Input(key='-IN-') 
+
+while True:
+    event, values = window.read()
+    print(event, values)
+      
+    if event == 'Search':
+        # Update the "output" text element
+        # to be the value of "input" element
+        window['-OUTPUT-'].update(values['-IN-'])
+
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    print(event, values)
+  
+window.close()
